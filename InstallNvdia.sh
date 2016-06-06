2243f40dcdaa4ca1aed205393ce4450c3f0bacfc
Mrlk <ymh521999@aliyun.com>
2016-06-06 01:19:56 -0400

    Tools

--- InstallNvdia.sh
+++ InstallNvdia.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+echo "Please Download Driver"
+read -p "Keyin download url: " downloadurl
+read -p "How many download processes do you want? : " downloadnu
+axel -n $downloadnu -a -o nvdia.run $downloadurl
+if $?=0
+	echo "Script Run Error !"
+	exit
+
+echo "Install Kernel Source Packages"
+yum install kernel-devel
 
