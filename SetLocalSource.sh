b1e4abbaa22d6cc3573ff4f86c1890b8fdde77a5
Mrlk <ymh521999@aliyun.com>
2016-06-06 00:15:41 -0400

    Tools

---
+++ SetLocalSource.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Script By lksoft
+# iso root path
+rootpath="/software_source"
+
+echo "The script must run super user mode !"
+echo "Please enter RHEL iso  path: "
+
+read isopath
+mkdir $rootpath
+mount $isopath $rootpath
+echo "
+[local_server]
+name=This is a local software source
+baseurl=file://$rootpath
+enabled=1
+gpgcheck=0
+" > /etc/yum.repos.d/LocalSource.repo
+
+echo " Do you want it auto start?(y/n)"
+
+read input1
+	case $input1 in
+	y | Y)
+		echo "$isopath	$rootpath	iso9660		defaults	1 2" >> /etc/fstab
+		;;
+	n | N)
+		echo "Finish Local Software Source Config !"
+		;;
+	*)
+		echo "Please Re-enter"
+		;;
+	esac
+#End
